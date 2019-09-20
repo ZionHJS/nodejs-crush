f51e5d20668c168d86afbbd5794331b6b6662192
zionHuang <hjszion@gmail.com>
2019-09-20 11:46:43 -0700

    added url-demo

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -10,4 +10,8 @@ console.log(path.dirname(__filename));
 console.log(path.extname(__filename));
 
 //Create path object
-console.log(path.parse(__filename));
+console.log(path.parse(__filename).base);
+
+//concatenate paths
+console.log(path.join(__dirname, 'test', 'hello.html'));
+
---
+++ reference/url_demo.js
@@ -0,0 +1,29 @@
+const url = require('url');
+const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
+
+//Serialized URL
+console.log("href:"+myUrl.href);
+console.log("toString:"+myUrl.toString());
+
+//Host (root domain)
+console.log("host:"+myUrl.host);  //host:mywebsite.com
+//Hostnname(does not get port)
+console.log("hostname:"+myUrl.hostname);   //hostname:mywebsite.com
+//Pathname
+console.log("pathname:"+myUrl.pathname);   //pathname:/hello.html
+//Serializaed query
+console.log("serialized:"+myUrl.search);   //serialized:?id=100&status=active
+
+//params object
+console.log("params:"+myUrl.searchParams);   //params:id=100&status=active
+//Add param
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);   //URLSearchParams { 'id' => '100', 'status' => 'active', 'abc' => '123' }
+//Loop through params
+myUrl.searchParams.forEach((value,name) => console.log(`${name}:${value}`));  
+// id:100
+// status:active
+// abc:123
+
+
+
