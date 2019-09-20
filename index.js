7d42ad930f4ecf49fa0c90ca8e37c30bc1fdcf73
zionHuang <hjszion@gmail.com>
2019-09-20 12:21:08 -0700

    added http

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
-logger.on('message', (data) => console.log('Called Listener:', data));
+const server = http.createServer((req, res) => {
+    // console.log(req.url);  //this shows current req.url   http://localhost:5000/about => about
+    if(req.url === '/'){
+        res.end('<h1>HomePage</h1>');
+    }
+});
 
-logger.log('Hello World!');
+const PORT = process.env.PORT || 5000;
 
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
--- logger.js
+++ logger.js
@@ -4,8 +4,13 @@ const uuid = require('uuid');
 class Logger extends EventEmitter{
     log(msg){
         //Call event
-        this.emit('message', {id:uuid.v4(), msg});
+        this.emit('message', {id:uuid.v4(), msg:msg});
     }
 }
 
-module.exports = Logger;
+// module.exports = Logger;
+
+const logger = new Logger();
+logger.on('message', (data) => console.log('Called Listener:', data));
+
+logger.log('Hello World!');
